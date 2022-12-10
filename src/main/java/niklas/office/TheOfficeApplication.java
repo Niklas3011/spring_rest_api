5ad4da7c98f38328b69880c797a30ab9353c2b15
Niklas3011 <109971037+Niklas3011@users.noreply.github.com>
2022-12-10 21:20:10 +0100

    Update and rename TheOfficeUsApplication.java to TheOfficeApplication.java

--- src/main/java/niklas/office/TheOfficeApplication.java
+++ src/main/java/niklas/office/TheOfficeApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TheOfficeUsApplication {
-
-    //private CarService service;
+public class TheOfficeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TheOfficeUsApplication.class, args);
